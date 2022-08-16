8bcb9ae4679af879bf158b7f50aa88b3ae6e2f13
sajid-khan-js <sajid.khan1@sainsburys.co.uk>
2022-08-16 12:53:41 +0100

    feat(k8sclient): auto discover local or incluster

--- go-app/go.mod
+++ go-app/go.mod
@@ -4,13 +4,13 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/gkarthiks/k8s-discovery v0.23.1
 	github.com/stretchr/testify v1.8.0
 	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a
 	github.com/swaggo/gin-swagger v1.5.2
 	github.com/swaggo/swag v1.8.4
 	k8s.io/api v0.24.3
 	k8s.io/apimachinery v0.24.3
-	k8s.io/client-go v0.24.3
 )
 
 require (
@@ -32,8 +32,8 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/imdario/mergo v0.3.5 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -44,6 +44,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
@@ -59,8 +60,10 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/client-go v0.24.3 // indirect
 	k8s.io/klog/v2 v2.60.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
+	k8s.io/metrics v0.23.1 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
--- go-app/modules/k8sclient/k8sclient.go
+++ go-app/modules/k8sclient/k8sclient.go
@@ -3,42 +3,26 @@ package k8sclient
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
 
+	discovery "github.com/gkarthiks/k8s-discovery"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
-func InitClient() (*kubernetes.Clientset, error) {
+func InitClient() (*discovery.K8s, error) {
 
-	clientSet := &kubernetes.Clientset{}
-
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, fmt.Errorf("InitClient: error getting user home dir: %w", err)
-	}
-
-	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
-
-	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		return nil, fmt.Errorf("InitClient: error getting Kubernetes config: %w", err)
-	}
-
-	clientSet, err = kubernetes.NewForConfig(kubeConfig)
+	// https://medium.com/swlh/clientset-module-for-in-cluster-and-out-cluster-3f0d80af79ed
+	k8sClients, err := discovery.NewK8s()
 	if err != nil {
 		return nil, fmt.Errorf("InitClient: error getting Kubernetes clientset: %w", err)
 	}
 
-	return clientSet, nil
+	return k8sClients, nil
 }
 
-func GetNamespaces(clientSet kubernetes.Clientset) ([]string, error) {
+func GetNamespaces(k8sClients discovery.K8s) ([]string, error) {
 
-	namespaces, err := clientSet.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	namespaces, err := k8sClients.Clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("GetNamespaces: %w", err)
 	}
@@ -52,15 +36,15 @@ func GetNamespaces(clientSet kubernetes.Clientset) ([]string, error) {
 	return currentNamespaces, nil
 }
 
-func GetPods(clientSet kubernetes.Clientset, namespace string) (map[string]string, error) {
+func GetPods(k8sClients discovery.K8s, namespace string) (map[string]string, error) {
 
 	// Check namespace exists first
-	_, err := clientSet.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
+	_, err := k8sClients.Clientset.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("GetPods: %w", err)
 	}
 
-	pods, err := clientSet.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
+	pods, err := k8sClients.Clientset.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("GetPods: failed to get Pods from K8s API: %w", err)
 	}
@@ -74,7 +58,7 @@ func GetPods(clientSet kubernetes.Clientset, namespace string) (map[string]strin
 	return currentPods, nil
 }
 
-func CreateNamespace(clientSet kubernetes.Clientset, name string) error {
+func CreateNamespace(k8sClients discovery.K8s, name string) error {
 
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
@@ -82,7 +66,7 @@ func CreateNamespace(clientSet kubernetes.Clientset, name string) error {
 		},
 	}
 
-	_, err := clientSet.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
+	_, err := k8sClients.Clientset.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
 	if err != nil {
 		return fmt.Errorf("CreateNamespaces: %w", err)
 	}
